45025a1cba5fca91c5b4a32476023b3a0c375817
PratikBhandare <146208370+PratikBhandare@users.noreply.github.com>
2025-03-07 14:40:07 +0530

    Delete NodeJs/REST Assignment directory

--- NodeJs/REST Assignment/dist/Repository/anagerRepo.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-class ManagerRepo {
-}
-module.exports = new ManagerRepo();
--- NodeJs/REST Assignment/src/Routes/AdminRoutes.ts
+++
@@ -1,9 +0,0 @@
-
-import express from "express";
-const AdminController = require("../controllers/AdminController")
-const router = express.Router();
-
-router.get("/leaves/report",AdminController.getReport)
-
-
-module.exports=router;
